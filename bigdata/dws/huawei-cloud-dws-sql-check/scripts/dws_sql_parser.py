b379a7219c812740ef6a7db93a36a78c4a2925ef
lccode <akuvwA9g6F7X8MUd.lccode@noreply.gitcode.com>
2026-07-21 03:11:08 +0800

    update skill: bigdata/dws/huawei-cloud-dws-sql-check [v1.0.3]

--- bigdata/dws/huawei-cloud-dws-sql-check/scripts/dws_sql_parser.py
+++ bigdata/dws/huawei-cloud-dws-sql-check/scripts/dws_sql_parser.py
@@ -19,7 +19,7 @@ from grammar_rules import (
 )
 from keywords import is_keyword, is_reserved_keyword, KeywordCategory
 
-# sdfsadfdsafdsaf
+# sdfsadfdsafdsaf22222222
 class SyntaxError:
     """Represents a syntax error in SQL text"""
 
